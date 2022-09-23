d28889dcbc4f91247255852d4e4b77b3ca0d8090
Abhradeep Barman <abhradeep709@gmail.com>
2022-09-23 20:54:59 +0530

    dma added

---
+++ C code/9- Dynamic Memory Allocation/realloc.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int *ptr;
+    ptr = (int *) calloc(5, sizeof(int));
+
+    printf("enter number(5): ");
+    for(int i=0; i<5; i++){
+        scanf("%d", &ptr[i]);
+    }
+    
+    ptr = realloc(ptr, 8);   // now size of ptr -> 8
+
+    printf("enter number(8): ");
+    for(int i=0; i<8; i++){
+        scanf("%d", &ptr[i]);
+    }
+
+    //printing the elements
+    for(int i=0; i<8; i++){
+        printf("%d ", ptr[i]);
+    }
+
+    return 0;
+}
